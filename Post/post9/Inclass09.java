79e3953d3c1df3143652af16571116ef65c28379
Bas <bas10600@gmail.com>
2016-11-28 09:21:35 +0700

    Inclass 9

---
+++ Post/post9/Inclass09.java
@@ -0,0 +1,15 @@
+class Inclass09{
+	public static void main(String[] args){
+		int a[] = new int[5];
+		int b[] = {12, 85, 96 ,78 ,12};
+		
+		System.out.print("Array a have elements are ");
+		for(int num : a){
+			System.out.print(num+" ");
+		}
+		System.out.print("\nArray b have elements are ");
+		for(int num : b){
+			System.out.print(num+" ");
+		}
+	}
+}
